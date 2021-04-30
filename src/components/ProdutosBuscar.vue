4800087a05025342ded3905afd7e7edb917c8858
Erik Danilo <eriikd@hotmail.com>
2021-04-30 17:35:43 -0300

    Area de Buscar

--- src/components/ProdutosBuscar.vue
+++ src/components/ProdutosBuscar.vue
@@ -1,9 +1,33 @@
 <template>
-  <input type="text" />
+  <form>
+    <input type="text" name="busca" id="busca" v-model="busca" />
+    <input
+      type="submit"
+      name="lupa"
+      id="lupa"
+      value="Buscar"
+      @click.prevent="buscarProdutos"
+    />
+  </form>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      busca: "",
+    };
+  },
+  methods: {
+    buscarProdutos() {
+      this.$router.push({
+        query: {
+          q: this.busca,
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style scoped></style>
