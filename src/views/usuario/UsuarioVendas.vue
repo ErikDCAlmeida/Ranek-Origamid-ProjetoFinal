7f34a4dc7e344afe0c1789f89941db629decf6fa
Erik Danilo <eriikd@hotmail.com>
2021-05-02 21:57:48 -0300

    Finalização projeto, criação visualização das compras e vendas feitas.

--- src/views/usuario/UsuarioVendas.vue
+++ src/views/usuario/UsuarioVendas.vue
@@ -1,9 +1,83 @@
 <template>
-  <p>Lista de Vendas</p>
+  <section>
+    <div v-if="vendas">
+      <h2>Vendas</h2>
+      <div class="produtoWrapper" v-for="(venda, index) in vendas" :key="index">
+        <ProdutoItem v-if="venda.produto" :produto="venda.produto">
+          <p class="comprador">
+            <span>Comprador:</span>
+            {{ venda.compradorId }}
+          </p>
+        </ProdutoItem>
+        <div class="entrega">
+          <h3>Entrega:</h3>
+          <ul v-if="venda.endereco">
+            <li v-for="(value, key) in venda.endereco" :key="key">
+              {{ key }}: {{ value }}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoItem from "@/components/ProdutoItem.vue";
+import { api } from "@/services.js";
+import { mapState } from "vuex";
+
+export default {
+  name: "UsuarioVendas",
+  data() {
+    return {
+      vendas: null,
+    };
+  },
+  components: {
+    ProdutoItem,
+  },
+  computed: {
+    ...mapState(["usuario", "login"]),
+  },
+  methods: {
+    getVendas() {
+      api.get(`/transacao?vendedorId=${this.usuario.id}`).then((response) => {
+        this.vendas = response.data;
+      });
+    },
+  },
+  watch: {
+    login() {
+      this.getVendas();
+    },
+  },
+  created() {
+    if (this.login) {
+      this.getVendas();
+    }
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+.produtoWrapper {
+  margin-bottom: 40px;
+}
+.comprador span {
+  color: #e80;
+}
+h2 {
+  margin-bottom: 20px;
+}
+.entrega {
+  display: grid;
+  grid-template-columns: minmax(100px, 200px) 1fr;
+  gap: 20px;
+  margin-bottom: 60px;
+}
+h3 {
+  justify-self: end;
+  margin: 0;
+}
+</style>
